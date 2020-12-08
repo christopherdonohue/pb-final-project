8e60990f1ef04cc5dc5a54abff66aa4750c3e2bd
Chris Donohue <cdonohu4@uncc.edu>
2020-12-08 04:19:26 -0500

    one

--- server/server.js
+++ server/server.js
@@ -38,7 +38,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 let corsOptions = {
-  origin: 'https://safe-woodland-35734.herokuapp.com/',
+  origin: 'http://localhost:4200',
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
